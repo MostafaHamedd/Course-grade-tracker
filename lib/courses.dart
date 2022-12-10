33cd57f0378f11140b586b29808ddd21cffc8049
Tellmore01 <88238579+Tellmore01@users.noreply.github.com>
2022-12-09 18:08:15 -0600

    Courses page updated

--- lib/courses.dart
+++ lib/courses.dart
@@ -1,31 +1,73 @@
 import 'package:flutter/material.dart';
-import 'package:tellmore_course_tracker/reusableWidgets.dart';
-import 'reusableWidgets.dart';
-class CoursesPage extends StatefulWidget {
-  const CoursesPage({Key? key}) : super(key: key);
 
+class CoursesPage extends StatefulWidget {
   @override
-  State<CoursesPage> createState() => _CoursesPageState();
+  _CoursesPageState createState() => _CoursesPageState();
 }
 
 class _CoursesPageState extends State<CoursesPage> {
+  final List<String> _courses = ["Course 1", "Course 2", "Course 3"];
+  String _newCourse = "";
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: ReusableWidgets().appBar('My Courses'),
-      body: coursesBody(),
+    return Material(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              itemCount: _courses.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  color: Colors.primaries[index % Colors.primaries.length],
+                  child: ListTile(
+                    title: Text(_courses[index]),
+                    onTap: () {
+                      // Do something when the tile is clicked
+                      // Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute(
+                      //     builder: (context) => AddAssignmentsScreen(course: _courses[index]));
+                    },
+                    trailing: IconButton(
+                      icon: Icon(Icons.add),
+                      onPressed: () {
+                        _courses.add(_newCourse);
+                        _newCourse = "";
+                        // Do something when the button is clicked
+                      },
+                    ),
+                  ),
+                );
+              },
+            ),
+
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: "Add a new course",
+              ),
+              onChanged: (value) {
+                _newCourse = value;
+              },
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              // Set the button color to black
+            ),
+            child: Text("Add Course"),
+            onPressed: () {
+              setState(() {
+                _courses.add(_newCourse);
+              });
+            },
+          ),
+        ],
+      ),
     );
   }
 }
 
-Widget coursesBody(){
-  return Center(
-    child: Column(
-      children: [
-        Container(
-          child: Text('Course 1'),
-        ),
-      ],
-    ),
-  );
-}
